57a5a9023efdbf4920b69c38cd13f9a49ec81b00
Sibonisiwe Sodinga <sodinga91@gmail.com>
2020-08-07 15:23:10 +0200

    added a read me file

--- test/count-reg-number.test.js
+++ test/count-reg-number.test.js
@@ -6,7 +6,7 @@ describe('The count reg number function', function() {
         var regCount = countRegNumber('CY 0987, CA 3456, ND 24680');
         assert.equal(3, regCount);
     });
-    it('should return 5 for reg numbers CY 0987, CA 3456, ND 2680', function() {
+    it('should return 5 for reg numbers CY 0987, CA 3456, ND 2680, CL 200, CA 390', function() {
         var regCount = countRegNumber('CY 0987, CA 3456, ND 24680, CL 200, CA 390');
         assert.equal(5, regCount);
     });
--- test/reg-check.test.js
+++ test/reg-check.test.js
@@ -1,15 +1,15 @@
 let assert = require("assert");
 let regCheck = require("../reg-check");
 
-describe('The regCheck function', function() {
-    it('should return true for reg numbers that end with GP', function() {
+describe('The regCheck function', function () {
+    it('should return true for reg numbers that end with GP', function () {
         var gautengReg = '12345 GP';
         assert.equal(true, regCheck(gautengReg, 'GP'));
     });
 
-    it('should return false for ND', function() {
+    it('should return false for ND', function () {
         var durbanReg = 'ND 12345';
-        assert.equal(false, regCheck(durbanReg, 'ND')); 
+        assert.equal(false, regCheck(durbanReg, 'ND'));
 
-});
+    });
 });
